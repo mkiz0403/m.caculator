bfc1cf7ef3ad301673ef36364d472f41a7f67b7f
Taejeong <mkiz0403@gmail.com>
2025-09-02 23:56:10 +0900

    feat : pwa 설치및 설정 적용

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,36 @@
-import type { NextConfig } from "next";
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	disable: process.env.NODE_ENV === 'development', // 개발 환경에서는 PWA 비활성화
+	register: true,
+	skipWaiting: true,
+});
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+	reactStrictMode: true,
+	transpilePackages: ['@pqina/pintura', '@pqina/react-pintura'],
+	// SSR 관련 설정
+	experimental: {
+		esmExternals: 'loose',
+	},
+	// 정적 내보내기 비활성화 (SSR 문제 해결)
+	output: 'standalone',
+	webpack(config: {
+		module: { rules: { test: RegExp; issuer: RegExp; use: string[] }[] };
+		resolve: { fallback: any };
+	}) {
+		config.module.rules.push({
+			test: /\.svg$/i,
+			issuer: /\.[jt]sx?$/,
+			use: ['@svgr/webpack'],
+		});
+
+		config.resolve.fallback = {
+			...config.resolve.fallback,
+			canvas: false,
+		};
+
+		return config;
+	},
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
