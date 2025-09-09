f32f2817add462eb8bd66ddd51d3e5c4940b2394
Taejeong <mkiz0403@gmail.com>
2025-09-09 14:31:36 +0900

    feat : UTM적용한 리다이렉트 코드 생성

---
+++ src/app/r/[slug]/page.tsx
@@ -0,0 +1,26 @@
+import { redirect } from 'next/navigation';
+
+const redirects: Record<string, string> = {
+	//네이버 다모아999
+	damoa999:
+		'https://saltboy.store/?utm_source=cafe&utm_medium=community&utm_campaign=launch_share&utm_content=9hojin',
+	//네이버 정가거부
+	wjdrkrjqn:
+		'https://saltboy.store/?utm_source=cafe&utm_medium=community&utm_campaign=launch_share&utm_content=wjdrkrjqn',
+	//다모앙
+	damoang:
+		'https://saltboy.store/?utm_source=cafe&utm_medium=community&utm_campaign=launch_share&utm_content=damoang',
+	//스레드
+	thread:
+		'https://saltboy.store/?utm_source=thread&utm_medium=social&utm_campaign=launch_share&utm_content=thread',
+	//인스타
+	instagram:
+		'https://saltboy.store/?utm_source=instagram&utm_medium=social&utm_campaign=launch_share&utm_content=instagram',
+	naverblog:
+		'https://saltboy.store/?utm_source=naverblog&utm_medium=social&utm_campaign=launch_share&utm_content=naverblog',
+};
+
+export default function RedirectPage({ params }: { params: { slug: string } }) {
+	const url = redirects[params.slug] ?? 'https://saltboy.store';
+	redirect(url);
+}
